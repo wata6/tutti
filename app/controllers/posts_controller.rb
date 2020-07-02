6b43d15bfb973fc0e037c80b950a934d9f9fe8ed
kenta.watanabe <boc.watyannn@gmail.com>
2020-07-02 07:42:20 +0000

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     if logged_in?
@@ -28,11 +29,21 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: posts_path)
   end
   
   private
 
   def post_params
     params.require(:post).permit(:title,:region,:instrument_requirement,:content)
+  end
+  
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    unless @post
+      redirect_to posts_path
+    end
   end
 end
